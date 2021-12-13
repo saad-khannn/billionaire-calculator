63a8fbc6e0dbd60fc6604e87a028e369c8e1fcfd
Saad Khan <saadkhan12313@yahoo.com>
2021-12-13 00:12:12 -0500

    use api to show all ranks, names, and net worths

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-function calculate(){
-    console.log("button clicked");
+function getInputs(){
+    getBillionaires();
+}
+
+function getBillionaires(){
+    fetch('https://forbes400.herokuapp.com/api/forbes400/getAllBillionaires')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '';
+        data.forEach(function(billionaire){
+            netWorth = (billionaire.finalWorth) * 1000000
+            dollarValue = moneyToUsd(netWorth);
+            output += `
+                <div>
+                    <p>${billionaire.rank}) 
+                    ${billionaire.person.name} = 
+                    ${dollarValue}</p>
+                </div>
+            `
+        });
+        document.getElementById('information').innerHTML = output;
+    })
+}
+
+function moneyToUsd(number) {
+    return number.toLocaleString('en-US', 
+    { 
+        style: 'currency', 
+        currency: 'USD' 
+    });
 }
