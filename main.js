211f3a48f7464372cce4c096bbafdcdb0b5e7a91
Saad Khan <saadkhan12313@yahoo.com>
2021-12-13 23:34:25 -0500

    compares wealth to closest billionaire

--- main.js
+++ main.js
@@ -1,59 +1,70 @@
-window.onload = function () {
-    var btn = document.getElementById("btn");
-    if (btn.addEventListener) {
-        btn.addEventListener("click", calculate, false);
-    } 
-    else if (btn.attachEvent) {
-        btn.attachEvent("onclick", calculate);
-    }
-};
-
 function calculate() {
     yearsInput = document.getElementById("years").value;
     rateInput = document.getElementById("rate").value;
     totalAmount = rateInput * 40 * 52.1429 * yearsInput;
-    totalAmount = moneyToUsd(totalAmount);
-    
+    dollarAmount = moneyToUsd(totalAmount);
+
     displayWealth();
 }
 
-function displayWealth(){
+function displayWealth() {
     let wealthOutput = `After working for ${yearsInput} years at $${rateInput} an hour,
                         with the standard 40-hour work week,
-                        you would accumulate a wealth of ${totalAmount}.
+                        you would accumulate a wealth of ${dollarAmount}.
                         `;
     document.getElementById("user-wealth").innerHTML = wealthOutput;
 
-    getBillionaires();
+    consoleResult();
 }
 
-function getBillionaires(){
-    fetch('https://forbes400.herokuapp.com/api/forbes400/getAllBillionaires')
-    .then((res) => res.json())
-    .then((data) => {
-        let billionaireOutput = '';
-        data.forEach(function(billionaire){
-            netWorth = (billionaire.finalWorth) * 1000000
-            dollarValue = moneyToUsd(netWorth);
-            billionaireOutput += `
-                <div>
-                    <p>${billionaire.rank}) 
-                    ${billionaire.person.name} = 
-                    ${dollarValue}</p>
-                </div>
-                `
-        });
-        document.getElementById("information").innerHTML = billionaireOutput;
-    })
-    .catch((err) => console.log(err));
+async function getBillionaires() {
+    const response = await fetch('https://forbes400.herokuapp.com/api/forbes400/getAllBillionaires');
+    const data = await response.json();
+    return data;
+}
+
+var billionaireInfo = {};
+
+function compareWealths(data) {
+    var billionaireNames = [];
+    var billionaireWorths = [];
+    var billionaireName;
+    var billionaireWorth;
+
+    for (let i = 0; i < data.length; i++) {
+        netWorth = (data[i].finalWorth) * 1000000;
+        billionaireNames.push(data[i].person.name);
+        billionaireWorths.push(netWorth);
+    }
+
+    for (let i = 0; i < billionaireWorths.length; i++) {
+        if (billionaireWorths[i] < totalAmount) {
+            billionaireName = billionaireNames[i - 1];
+            billionaireWorth = billionaireWorths[i - 1];
+            break;
+        }
+    }
+    billionaireInfo.billionaireName = billionaireName;
+    billionaireInfo.billionaireWorth = moneyToUsd(billionaireWorth);
+}
+
+function showBillionaireInfo(){
+    let informationOutput = `You would still have less than wealth than ${billionaireInfo.billionaireName}
+                            who has a net worth of ${billionaireInfo.billionaireWorth}.
+                            `
+    document.getElementById("billionaire-information").innerHTML = informationOutput;
+}
+
+function consoleResult() {
+    getBillionaires().then(compareWealths).then(showBillionaireInfo);
 }
 
 function moneyToUsd(number) {
-    return number.toLocaleString('en-US', 
-    { 
-        style: 'currency', 
-        currency: 'USD',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0
-    });
+    return number.toLocaleString('en-US',
+        {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        });
 }
